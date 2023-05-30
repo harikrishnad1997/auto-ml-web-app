5cc61d4e3dae1298f303942cd3e61c6acb7180d4
Harikrishna Dev <hdev2@daifukuna.com>
2023-05-30 10:39:53 -0400

    pycaret changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from operator import index
 import streamlit as st
 import plotly.express as px
-from pycaret.regression import setup, compare_models, pull, save_model, load_model
+# from pycaret.regression import setup, compare_models, pull, save_model, load_model
 import pandas_profiling
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
@@ -32,13 +32,13 @@ if choice == "Profiling":
 if choice == "Modelling": 
     chosen_target = st.selectbox('Choose the Target Column', df.columns)
     if st.button('Run Modelling'): 
-        setup(df, target=chosen_target, silent=True)
-        setup_df = pull()
-        st.dataframe(setup_df)
-        best_model = compare_models()
-        compare_df = pull()
-        st.dataframe(compare_df)
-        save_model(best_model, 'best_model')
+        # setup(df, target=chosen_target, silent=True)
+        # setup_df = pull()
+        # st.dataframe(setup_df)
+        # best_model = compare_models()
+        # compare_df = pull()
+        # st.dataframe(compare_df)
+        # save_model(best_model, 'best_model')
 
 if choice == "Download": 
     with open('best_model.pkl', 'rb') as f: 
