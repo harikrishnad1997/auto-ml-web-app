723e461cb66c1dd2444aaccdd3f87af6ab07deda
Harikrishna Dev <37830351+harikrishnad1997@users.noreply.github.com>
2024-04-09 17:56:21 -0500

    Edits to streamlit app

--- app.py
+++ app.py
@@ -2,78 +2,90 @@ from operator import index
 import streamlit as st
 import plotly.express as px
 import numpy as np
-from pycaret.regression import setup, compare_models, pull, save_model, load_model, plot_model
-# from lazypredict.Supervised import LazyRegressor
-# from sklearn.model_selection import train_test_split
-from pandas_profiling import ProfileReport
+from lazypredict.Supervised import LazyRegressor, LazyClassifier
+from sklearn.model_selection import train_test_split
 import pandas as pd
-from streamlit_pandas_profiling import st_profile_report
-import os 
+from autoviz.AutoViz_Class import AutoViz_Class
+from sklearn.datasets import load_iris
+import os
 
-@st.cache
+@st.cache_resource
 def load_data():
-    return pd.read_csv('dataset.csv', index_col=None)
+    X, y = load_iris(return_X_y=True)
+    df = pd.DataFrame(X, columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
+    df['target'] = y
+    return df
 
-# Rest of your code...
+df = load_data()
 
-if os.path.exists('./dataset.csv'): 
-    df = load_data()
-
-with st.sidebar: 
-    st.image("https://michael-fuchs-python.netlify.app/post/2022-01-01-automl-using-pycaret-classification_files/p133s1.png")
+with st.sidebar:
+    st.image("https://analyticsindiamag.com/wp-content/uploads/2020/10/lazypredict.jpg")
     st.title("AutoML")
-    choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
+    choice = st.radio("Navigation", ["Upload", "Profiling", "Modelling", "Download"])
     st.info("This project application helps you build and explore your data.")
 
 if choice == "Upload":
     st.title("Upload Your Dataset")
     file = st.file_uploader("Upload Your Dataset")
-    if file: 
+    if file:
         df = pd.read_csv(file, index_col=None)
         df.to_csv('dataset.csv', index=None)
         st.dataframe(df)
 
-if choice == "Profiling": 
+if choice == "Profiling":
     st.title("Exploratory Data Analysis")
-    profile_df = df.profile_report()
-    st_profile_report(profile_df)
+    # AV = AutoViz_Class()
+    # config = {
+    #     'filename': None,
+    #     'sep': ',',
+    #     'depVar': '',
+    #     'dfte': df,
+    #     'header': 0,
+    #     'verbose': 0,
+    #     'lowess': False,
+    #     'chart_format': 'html',
+    #     'max_rows_analyzed': 10000,
+    #     'max_cols_analyzed': 50,
+    #     # 'save_plot_path': None,
+    # }
+    # AV.AutoViz()
+    # st.components.v1.html(AV.html, width=1000, height=1000, scrolling=True)
+    
+    AV = AutoViz_Class()
+    AV.AutoViz("",dfte=df)
+    report_path = os.path.join(os.path.dirname(__file__), "autoviz_report.html")
+    try:
+        AV.save_html(report_path)
+        HtmlFile = open(report_path, 'r', encoding='utf-8')
+        source_code = HtmlFile.read()
+        st.components.v1.html(source_code, width=1000, height=1000, scrolling=True)
+    except:
+        st.write("Error occurred while generating the AutoViz report.")
 
-if choice == "Modelling": 
-    chosen_target = st.selectbox('Choose the Target Column', df.columns)
-    if st.button('Run Modelling'): 
-        print("WIP")
-        # h2o.init()
-        # df = h2o.import_file(df)
-        # df.describe(chunk_summary=True)
-        # train, test = df.split_frame(ratios=[0.8], seed = 1)
-        # aml = H2OAutoML(max_models =25,
-        #         balance_classes=True,
-		# seed =16548846)
-        # aml.train(training_frame = train, y = 'y')
-        # lb = aml.leaderboard
-        # lb.head(rows=lb.nrows)
-        # aml.train(training_frame = train, y = 'y', leaderboard_frame = my_leaderboard_frame)
-        # best_model = aml.get_best_model()
-        # model_path = h2o.save_model(model=best_model,force=True)
-        setup(df.dropna(subset=chosen_target), target=chosen_target, session_id = 2774764,imputation_type = 'simple',numeric_imputation='mean',categorical_imputation='mode')
-        setup_df = pull()
-        st.dataframe(setup_df)
-        best_model = compare_models(n_select = 5)
-        compare_df = pull()
-        st.dataframe(compare_df)
-        plot_model(best_model, plot='residuals', display_format='streamlit')
-        plot_model(best_model, plot='feature', display_format='streamlit')
-        plot_model(best_model, plot='error', display_format='streamlit')
-        save_model(best_model, 'best_model')
-        # y = df[chosen_target]
-        # X = df.loc[:, df.columns!=chosen_target]
-        # X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.5,random_state =65481254)
-        # reg = LazyRegressor(verbose=0,ignore_warnings=False, custom_metric=None )
-        # models,predictions = reg.fit(X_train, X_test, y_train, y_test)
-        # st.dataframe(models)
-        # model_dictionary = reg.provide_models(X_train,X_test,y_train,y_test)
 
-if choice == "Download": 
-    print("Working")
-    with open('best_model.pkl', 'rb') as f: 
-        st.download_button('Download Model', f, file_name="best_model.pkl")
+if choice == "Modelling":
+    target_options = df.columns.tolist()
+    chosen_target = st.selectbox('Choose the Target Column', target_options)
+    model_type = st.selectbox('Choose Model Type', ['Regression', 'Classification'])
+    if st.button('Run Modelling'):
+        X = df.drop(chosen_target, axis=1)
+        y = df[chosen_target]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=65481254)
+
+        if model_type == 'Classification':
+            # Classification
+            clf = LazyClassifier(verbose=0, ignore_warnings=False, custom_metric=None)
+            models, predictions = clf.fit(X_train, X_test, y_train, y_test)
+            model_dictionary = clf.provide_models(X_train, X_test, y_train, y_test)
+        else:
+            # Regression
+            reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
+            models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+            model_dictionary = reg.provide_models(X_train, X_test, y_train, y_test)
+
+        st.dataframe(models)
+
+if choice == "Download":
+    with open('model_dictionary.pkl', 'wb') as f:
+        st.download_button('Download Model', f, file_name="model_dictionary.pkl")
